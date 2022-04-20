a4249d9f4d3468c50b6ed85986a711bd1a8b6b5c
arc06e <91097715+arc06e@users.noreply.github.com>
2022-04-20 11:04:23 -0400

    configured identity for identity users - injected UserManager and SignInManager. Added login and register views/AccountController. Updated DbContext for IdentityDbContext. Added Partial view for displaying login info on navbar. Removed first and last name from registration form.

---
+++ SocialClubApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialClubApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        //explain this
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ SocialClubApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialClubApp.ViewModels
+{
+    //create a view model when a model object does not contain all the data a view needs
+    //or, conversely, when you don't need all data represented in the model object
+    //a view model contains all the data a view needs 
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password",
+            ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
